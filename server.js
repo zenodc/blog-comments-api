481ef1a3194d23355e6cd8252e772f29a084d5eb
zenodc <120054096+zenodc@users.noreply.github.com>
2025-12-30 20:44:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -9,6 +10,14 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API commenti attiva");
 });
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connesso"))
+.catch(err => console.error("Errore connessione MongoDB:", err));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
