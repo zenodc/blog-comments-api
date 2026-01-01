04214a49987db5204262778e4275ae0b6c974373
zenodc <120054096+zenodc@users.noreply.github.com>
2026-01-01 14:38:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
 
-const express = require("express");
-const cors = require("cors");
-const authRoutes = require("./routes/auth");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Blog API is running");
+app.get('/', (req, res) => {
+  res.send('Blog API is running');
 });
 
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connesso"))
-.catch(err => console.error("Errore connessione MongoDB:", err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connesso'))
+  .catch(err => console.error('Errore connessione MongoDB:', err));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server in ascolto sulla porta", ${PORT});
+  console.log(`Server in ascolto sulla porta ${PORT}`);
 });
