9bf38576b963316c28bbb65de03a585304e563d8
zenodc <120054096+zenodc@users.noreply.github.com>
2026-01-01 14:20:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/auth', authRoutes);
 
 app.get("/", (req, res) => {
-  res.send("API commenti attiva");
+  res.send("Blog API is running");
 });
 
 
@@ -21,5 +25,5 @@ mongoose.connect(process.env.MONGO_URI, {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server in ascolto sulla porta", PORT);
+  console.log("Server in ascolto sulla porta", ${PORT});
 });
