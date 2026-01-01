5cecb8596f9024d59052a9edab6183423f37266d
zenodc <120054096+zenodc@users.noreply.github.com>
2026-01-01 15:20:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
-
 const app = express();
+const postsRoutes = require('./routes/posts');
 
 app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
+app.use('/posts', postsRoutes);
 
 app.get('/', (req, res) => {
   res.send('Blog API is running');
