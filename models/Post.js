0c21e33fb9e2a2e6837b4306e2232df3a8342ee6
zenodc <120054096+zenodc@users.noreply.github.com>
2026-01-07 13:35:55 +0100

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
 
+// sottoschema per i commenti
+const commentSchema = new mongoose.Schema(
+  {
+    author: { type: String, required: true },
+    text: { type: String, required: true },
+    date: { type: Date, default: Date.now }
+  },
+  {
+    _id: false // non ci serve un id per ogni singolo commento
+  }
+);
+
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
   slug: { type: String, required: true, unique: true },
   excerpt: String,
   content: String,
+
+  // 👇 NUOVO CAMPO
+  comments: [commentSchema],
+
   createdAt: { type: Date, default: Date.now },
   updatedAt: Date,
   published: { type: Boolean, default: true }
